2637c765a6ada13322b017e32510cc04a489f618
Richie <molunorichie@gmail.com>
2023-08-21 07:50:59 +0100

    update server to alowo credentials

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,10 @@ function initMiddlewares(app: Application) {
         app.use(morgan('dev'))
     }
 
-    app.use(cors());
+    app.use(cors({
+        origin: 'http://localhost:5173',
+        credentials: true
+    }));
 
     app.use(express.json())
 
