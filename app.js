6e8ab9395a1676c51c24619d3caf2de7e98ccd4c
Lakshay <lakshayhalo@gmail.com>
2023-09-05 16:27:22 +0530

    cors change

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.use(
     credentials: true,
     origin: [
       "http://localhost:3000",
-      "http://localhost:8080",
-      "http://localhost:4200",
       "http://127.0.0.1:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
